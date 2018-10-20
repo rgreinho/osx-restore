6421027c398ed39555987c62b34c4608b08ee5d1
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-10-20 10:08:19 -0500

    Update brew software

--- setup/brew.sh
+++ setup/brew.sh
@@ -14,35 +14,94 @@ brew tap buo/cask-upgrade
 if ! brew update || [ "${OSXR_BREW_FORCE_INSTALL}" -eq 1 ] ;then
   brew install \
     bash-completion \
+    bower \
     brew-cask-completion \
+    c-ares \
+    chromedriver \
     coreutils \
+    docker \
     docker-completion \
+    docker-compose \
+    docker-machine \
+    e2fsprogs \
     editorconfig \
+    ext2fuse \
+    ext4fuse \
+    freetype \
+    gdbm \
     geckodriver \
+    gettext \
     git \
     git-extras \
+    glib \
     glide \
+    gmp \
+    gnutls \
     go \
+    gradle \
+    gradle-completion \
     htop \
     hub \
+    hugo \
+    icu4c \
     imagemagick \
+    jenv \
+    jpeg \
     jq \
+    kubernetes-cli \
+    kubernetes-helm \
     libffi \
+    libgcrypt \
+    libgpg-error \
+    libidn2 \
+    libmaxminddb \
+    libpng \
+    libtasn1 \
+    libtiff \
+    libtool \
+    libunistring \
+    libyaml \
     liquidprompt \
+    little-cms2 \
+    lua@5.1 \
+    makedepend \
+    md5sha1sum \
     most \
     multitail \
+    netcat \
+    nettle \
+    node \
     node@8 \
+    oniguruma \
+    openjpeg \
     openssl \
+    openssl@1.1 \
+    p11-kit \
     p7zip \
     pandoc \
+    pcre \
+    pcre2 \
+    perl \
     pip-completion \
+    pkg-config \
+    postgresql \
+    python \
     python3 \
+    python@2 \
+    readline \
+    redis \
     ruby \
+    s-lang \
     shellcheck \
+    sqlite \
+    terraform \
     tree \
     vagrant-completion \
+    vault \
     vim \
+    webp \
     wget \
+    wireshark \
     xz \
     yarn
   
@@ -71,3 +130,5 @@ if ! brew update || [ "${OSXR_BREW_FORCE_INSTALL}" -eq 1 ] ;then
   brew cleanup
   brew cask cleanup
 fi
+
+
